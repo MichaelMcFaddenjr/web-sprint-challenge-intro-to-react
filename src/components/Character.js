30487dff5659e84c48cdcf87f54a17c0a7184047
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-03-20 14:41:11 -0700

    Keep getting error of map undefined. Unable to resolve and working on saturday so no help channel. Fishined rest to best of my ability

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,19 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    font-size: 1rem;
+`
+const character = (props) => {
+    const {character} = props
+
+    return (
+        <Wrapper>
+            <h1>Name: {character.name}</h1>
+            <p>Birth year: {character.birth_year}</p>
+            <p>Gender: {character.gender}</p>
+        </Wrapper>
+    )
+}
+
+export default character
