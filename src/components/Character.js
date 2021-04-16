76a75ab75c86377555bbb8d1a576990296dcb516
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-04-16 12:38:20 -0600

    Completed MVP and styling

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,23 @@
-// Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const CardWrapper = styled.div`
+    background: white;
+    width: 40%;
+    margin: 2% auto;
+    padding: 1px;
+    border: 2px red solid;
+`
+
+
+
+function Character ({ character }) {
+    return(
+        <CardWrapper>
+            <h2>Name: {character.name}</h2>
+            <p>Height: {character.height}</p>
+            <p>Birth Year: {character.birth_year}</p>
+        </CardWrapper>
+    )
+}
+export default Character
